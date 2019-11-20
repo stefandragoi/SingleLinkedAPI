39699469cfd2cb9964d87428b370d0d903d947bf
Stefan Dragoi <Stefan.dragoi@gmail.com>
2019-11-20 18:09:13 +0200

    Implemented required functionalities. Working on a single thread. TODO: investigate sync problems, prettify code.

--- main.c
+++ main.c
@@ -4,27 +4,70 @@
  *  Created on: Nov 20, 2019
  *      Author: sdragoi
  */
-#include <stdio.h>
-#include <pthread.h>
+#include "utils.h"
 
-static void *show(void *arg)
-{
-	printf("This is thread %u\n", pthread_self());
+pthread_mutex_t mutex;
 
+void* thread1_job(void *arg) {
+	pthread_barrier_t *barrier;
+	barrier = (pthread_barrier_t*) arg;
+	pthread_barrier_wait(barrier);
+
+	add_node(&mutex, 2);
+	add_node(&mutex, 4);
+	add_node(&mutex, 10);
+	delete_node(&mutex, 2);
+	sort_list(&mutex);
+	delete_node(&mutex, 10);
+	delete_node(&mutex, 5);
 	return NULL;
 }
 
+void* thread2_job(void *arg) {
+	pthread_barrier_t *barrier;
+	barrier = (pthread_barrier_t*) arg;
+	pthread_barrier_wait(barrier);
+
+	add_node(&mutex, 11);
+	add_node(&mutex, 1);
+	delete_node(&mutex, 11);
+	add_node(&mutex, 8);
+	print_list(&mutex);
+	return NULL;
+}
+
+void* thread3_job(void *arg) {
+	pthread_barrier_t *barrier;
+	barrier = (pthread_barrier_t*) arg;
+	pthread_barrier_wait(barrier);
+
+	add_node(&mutex, 30);
+	add_node(&mutex, 25);
+	add_node(&mutex, 100);
+	sort_list(&mutex);
+	print_list(&mutex);
+	delete_node(&mutex, 100);
+	return NULL;
+
+}
+
 int main() {
-	printf("Hello World!\n");
+	pthread_barrier_t barrier;
+	pthread_barrier_init(&barrier, NULL, 2);
 
-	pthread_t ths[3];
-	int j;
+    pthread_mutex_init(&mutex, NULL);
 
-	for (j = 0; j < 3; j++)
-		pthread_create(&ths[j], NULL, &show, NULL);
+	pthread_t thread1, thread2, thread3;
+	pthread_create(&thread1, NULL, &thread1_job, (void*)&barrier);
+	pthread_create(&thread2, NULL, &thread2_job, (void*)&barrier);
+	pthread_create(&thread3, NULL, &thread3_job, (void*)&barrier);
 
-	for (j = 0; j < 3; j++)
-		pthread_join(ths[j], NULL);
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+	pthread_join(thread3, NULL);
 
+	pthread_barrier_destroy(&barrier);
+
+	printf("Done!\n");
 	return 0;
 }
---
+++ utils.c
@@ -0,0 +1,154 @@
+/*
+ * utils.c
+ *
+ *  Created on: Nov 20, 2019
+ *      Author: sdragoi
+ */
+
+#include "utils.h"
+
+void print_element(int x) {
+	printf("%d; ", x);
+}
+
+struct node* create_node(int data) {
+	struct node* new_node;
+
+	new_node = malloc(sizeof(struct node*));
+	new_node->next = NULL;
+	new_node->callback_print = print_element;
+	new_node->value = data;
+
+	return new_node;
+}
+
+struct node* head;
+
+void add_node(pthread_mutex_t* mutex, int data) {
+	struct node* prev_node;
+	struct node* new_node;
+
+    pthread_mutex_lock(mutex);
+	prev_node = head;
+	new_node = create_node(data);
+	if (NULL == new_node) {
+		printf("Thread no. %lu tried to add the following element: %d, malloc failed\n", (unsigned long)pthread_self(), data);
+		return;
+	}
+
+	if (NULL == prev_node) {
+		head = new_node;
+	} else {
+		while (NULL != prev_node->next)
+			prev_node = prev_node->next;
+
+		prev_node->next = new_node;
+	}
+
+    pthread_mutex_unlock(mutex);
+	printf("Thread no. %lu added the following element: %d\n", (unsigned long)pthread_self(), data);
+}
+
+void delete_node(pthread_mutex_t* mutex, int data) {
+	struct node* current_node;
+	struct node* prev_node;
+
+    pthread_mutex_lock(mutex);
+	current_node = head;
+
+	if (NULL == current_node) {
+		printf("Thread no. %lu tried to delete the following element, list is empty: %d\n", (unsigned long)pthread_self(), data);
+		return;
+	} else if (data == current_node->value) {
+		head = current_node->next;
+		free(current_node);
+		printf("Thread no. %lu deleted the following element: %d\n", (unsigned long)pthread_self(), data);
+	} else {
+		while (NULL != current_node) {
+			prev_node = current_node;
+			if (data == current_node->value)
+				break;
+			current_node = current_node->next;
+		}
+
+		if (NULL == current_node) {
+			printf("Thread no. %lu tried to delete the following unavailable element: %d\n", (unsigned long)pthread_self(), data);
+		} else {
+			prev_node->next = current_node->next;
+			free(current_node);
+			printf("Thread no. %lu deleted the following element: %d\n", (unsigned long)pthread_self(), data);
+		}
+	}
+    pthread_mutex_unlock(mutex);
+}
+
+void print_list(pthread_mutex_t* mutex) {
+	struct node* current_node;
+    pthread_mutex_lock(mutex);
+	current_node = head;
+	printf("Thread no. %lu is printing the list: ", (unsigned long)pthread_self());
+
+	while(NULL != current_node) {
+		current_node->callback_print(current_node->value);
+		current_node = current_node->next;
+	}
+	printf("\n");
+    pthread_mutex_unlock(mutex);
+}
+
+void sort_list(pthread_mutex_t* mutex) {
+	int count, i;
+	struct node* aux_node;
+	struct node* node_j;
+	struct node* prev_j;
+
+    pthread_mutex_lock(mutex);
+	aux_node = head;
+	count = 0;
+	while(NULL != aux_node) {
+		count++;
+		aux_node = aux_node->next;
+	}
+
+	for (i = 0; i < count - 1; i++) {
+		node_j = head;
+		prev_j = NULL;
+
+		while(NULL != node_j && NULL != node_j->next) {
+			if (node_j->value > node_j->next->value) {
+				if (prev_j == NULL) // swapping list's head, no previous element
+					head = node_j->next;
+				else
+					prev_j->next = node_j->next;
+
+				aux_node = node_j->next;
+				node_j->next = node_j->next->next;
+				aux_node->next = node_j;
+			}
+
+			// advance forward through the list
+			prev_j = node_j;
+			node_j = node_j->next;
+		}
+	}
+
+	printf("Thread no. %lu sorted the list\n", (unsigned long)pthread_self());
+    pthread_mutex_unlock(mutex);
+}
+
+void flush_list(pthread_mutex_t* mutex) {
+	struct node* current_node;
+	struct node* aux_node;
+
+    pthread_mutex_lock(mutex);
+	current_node = head;
+	head = NULL;
+	while(NULL != current_node) {
+		aux_node = current_node;
+		current_node = current_node->next;
+		free(aux_node);
+	}
+
+	printf("Thread no. %lu flushed the list\n", (unsigned long)pthread_self());
+    pthread_mutex_unlock(mutex);
+}
